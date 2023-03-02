48bebe552cdba31d7473725e64cb4641f748ae13
Noam Belleli <noamhoter@gmail.com>
2023-03-02 17:07:59 +0200

    Part II - API GET Routes: Return Unsplash Photos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const app = express();
 
@@ -10,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Unsplash API!" });
 });
+
+app.use("/api/photos", require('./routes/photoRoutes'));
 
 app.listen(port, () => console.log(`server started on port ${port}`));
--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const photoRouter = express.Router();
 const axios = require("axios");
-
+require("dotenv").config();
 
 const API = "https://api.unsplash.com/photos/";
 
@@ -11,7 +11,8 @@ photoRouter.route("/").get(async (req, res) => {
       `${API}?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
     );
     const photos = response.data;
-    res.status(200).send(photos);
+    const photosRawUrls = photos.map((photo) => photo.urls.raw)
+    res.status(200).send(photosRawUrls);
   } catch (error) {
     res.status(500).json({
       message: "Server error. Please try again later."
@@ -19,3 +20,22 @@ photoRouter.route("/").get(async (req, res) => {
     
   }
 });
+
+photoRouter.route("/:id").get(async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const response = await axios.get(
+      `${API}/${id}?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+    );
+    const photo = response.data;
+    res.status(200).send(photo);
+    
+  } catch (error){     
+      res.status(500).json({
+        message: "Server error. Please try again later."
+      })
+    }
+})
+
+module.exports = photoRouter;
